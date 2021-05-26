735c309ce7ec46577e901cea11b54e9cb8d66003
AdKurPur <84677344+AdKurPur@users.noreply.github.com>
2021-05-27 01:20:52 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/jst_service/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/AdKurPur/bot-jst/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
