8d151885a45f51b2c21f8e1f0b1cce52e88e0496
AdKurPur <84677344+AdKurPur@users.noreply.github.com>
2021-06-03 01:18:02 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -17,7 +17,7 @@ function ArgMax(res){
     for(i=0; i<res.length; i++){
         cls_data[i] = res[i]
     }
-    console.log(cls_data, argMax(cls_data)):
+    console.log(cls_data, argMax(cls_data));
     
     if(argMax(cls_data) == 1){
         label = "OVER VOLTAGE"
